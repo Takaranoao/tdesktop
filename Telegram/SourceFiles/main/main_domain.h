a7018decbae33af63f7f09e11ee274934ef41ea3
Takaranoao <takaranoao@gmail.com>
2026-05-23 19:55:38 -0700

    feat: raise account limit for custom build

--- Telegram/SourceFiles/main/main_domain.h
+++ Telegram/SourceFiles/main/main_domain.h
@@ -31,8 +31,8 @@ public:
 		std::unique_ptr<Account> account;
 	};
 
-	static constexpr auto kMaxAccounts = 3;
-	static constexpr auto kPremiumMaxAccounts = 6;
+	static constexpr auto kMaxAccounts = 99;
+	static constexpr auto kPremiumMaxAccounts = 99;
 
 	explicit Domain(const QString &dataName);
 	~Domain();
